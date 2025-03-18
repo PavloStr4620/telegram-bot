408af1e72cee1d5ec192db76408c4688e0f712f5
Pavlo <112697824+PavloStr4620@users.noreply.github.com>
2025-03-18 19:26:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,42 +28,43 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def send_welcome(message: types.Message):
     chat_id = message.chat.id
-    builder = InlineKeyboardBuilder()
+    # builder = InlineKeyboardBuilder()
+    await message.answer("Ласкаво просимо!")
 
-    if get_token(chat_id) is None:
-        builder.button(text='Вхід', callback_data='login')
-        builder.button(text='Реєстрація', callback_data='registration')
-    else:
-        if check_beehive_exists():
-            builder.button(text='Переглянути пасіку', callback_data='view_apiary')
-            builder.button(text='Створити пасіку', callback_data='create_apiary')
-        else:
-            builder.button(text='Створити пасіку', callback_data='create_apiary')
+    # if get_token(chat_id) is None:
+    #     builder.button(text='Вхід', callback_data='login')
+    #     builder.button(text='Реєстрація', callback_data='registration')
+    # else:
+    #     if check_beehive_exists():
+    #         builder.button(text='Переглянути пасіку', callback_data='view_apiary')
+    #         builder.button(text='Створити пасіку', callback_data='create_apiary')
+    #     else:
+    #         builder.button(text='Створити пасіку', callback_data='create_apiary')
 
-    await message.answer("Ласкаво просимо!", reply_markup=builder.as_markup())
+    # await message.answer("Ласкаво просимо!", reply_markup=builder.as_markup())
 
 
-@dp.callback_query(F.data.in_(['login', 'registration', 'create_apiary', 'view_apiary', 'back_in_menu']))
-async def callback_handler(call: types.CallbackQuery):
-    chat_id = call.message.chat.id
-    message_id = call.message.message_id
+# @dp.callback_query(F.data.in_(['login', 'registration', 'create_apiary', 'view_apiary', 'back_in_menu']))
+# async def callback_handler(call: types.CallbackQuery):
+#     chat_id = call.message.chat.id
+#     message_id = call.message.message_id
 
-    if call.data == 'login':
-        await bot.delete_message(chat_id, message_id)
-        await process_login(call.message)
-    elif call.data == 'registration':
-        await bot.delete_message(chat_id, message_id)
-        await process_registration(call.message)
-    elif call.data == 'create_apiary':
-        await bot.delete_message(chat_id, message_id)
-        await create_apiary(call.message)
-    elif call.data == 'view_apiary':
-        await bot.delete_message(chat_id, message_id)
-        await view_apiary(call.message)
-    elif call.data == 'back_in_menu':
-        await bot.delete_message(chat_id, message_id)
-        await send_welcome(call.message)
-    await call.answer() # Important to answer callback queries
+#     if call.data == 'login':
+#         await bot.delete_message(chat_id, message_id)
+#         await process_login(call.message)
+#     elif call.data == 'registration':
+#         await bot.delete_message(chat_id, message_id)
+#         await process_registration(call.message)
+#     elif call.data == 'create_apiary':
+#         await bot.delete_message(chat_id, message_id)
+#         await create_apiary(call.message)
+#     elif call.data == 'view_apiary':
+#         await bot.delete_message(chat_id, message_id)
+#         await view_apiary(call.message)
+#     elif call.data == 'back_in_menu':
+#         await bot.delete_message(chat_id, message_id)
+#         await send_welcome(call.message)
+#     await call.answer() # Important to answer callback queries
 
 async def main():
     print("Бот запущено...")
