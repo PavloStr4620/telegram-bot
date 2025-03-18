96ea2c24856d846fe5e64ebe500378a990d6ef98
Pavlo <112697824+PavloStr4620@users.noreply.github.com>
2025-03-18 18:50:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,15 @@
-import logging
-import os
+from flask import Flask,render_template
+from threading import Thread
 
-from aiogram import Bot, Dispatcher, types
-from aiogram.middlewares.logging import LoggingMiddleware # Виправлено імпорт
-from aiogram.utils import executor
+app = Flask(__name__)
 
-# Отримання токена бота з змінної середовища
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+@app.route('/')
+def index():
+    return "Alive"
 
-# Налаштування логування
-logging.basicConfig(level=logging.INFO)
+def run():
+  app.run(host='0.0.0.0',port=8080)
 
-# Ініціалізація бота та диспетчера
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot)
-
-# Додавання middleware для логування
-dp.middleware.setup(LoggingMiddleware())
-
-# Обробник команди /start
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    await message.reply("Привіт! Це тестовий бот на aiogram!")
-
-# Обробник всіх текстових повідомлень
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(f"Ти написав: {message.text}")
-
-# Основна функція для запуску бота
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+def keep_alive():  
+    t = Thread(target=run)
+    t.start()
