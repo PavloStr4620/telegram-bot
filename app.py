26044a86d3c91e98ba909dbe873f0eb75f7c51b1
Pavlo <112697824+PavloStr4620@users.noreply.github.com>
2025-03-18 14:50:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,52 @@
 import os
-import telebot
+import logging
 from flask import Flask, request
-
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import ParseMode
+from aiogram.utils.executor import start_webhook
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 WEBHOOK = os.getenv("WEBHOOK_URL")
 WEBHOOK_URL = f"{WEBHOOK}/{BOT_TOKEN}"
 
-bot = telebot.TeleBot(BOT_TOKEN)
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
 app = Flask(__name__)
 
+# Вебхук
+async def on_start():
+    await bot.set_webhook(WEBHOOK_URL)
+
+# Обробка команд
+@dp.message_handler(commands=['qwe'])
+async def handle_qwe(message: types.Message):
+    await message.reply("Ваша команда була отримана!")
+
+# Обробка запитів на вебхук
 @app.route('/' + BOT_TOKEN, methods=['POST'])
 def webhook():
-    print(f"Вебхук: {WEBHOOK_URL}")
     json_str = request.get_data().decode('UTF-8')
-    update = telebot.types.Update.de_json(json_str)
-    print(update)
-    bot.process_new_updates([update])
+    update = types.Update.parse_raw(json_str)
+    dp.process_update(update)
     return "!", 200
 
-@bot.message_handler(commands=['qwe'])
-def handle_qwe(message):
-    bot.reply_to(message, "Ваша команда була отримана!")
-
-
+# Домашня сторінка
 @app.route('/')
 def home():
     return "Bot is running!", 200
 
 if __name__ == '__main__':
+    # Налаштування вебхука перед запуском
     bot.remove_webhook()
     bot.set_webhook(url=WEBHOOK_URL)
-    app.run(host="0.0.0.0", port=5000)
-    
+
+    # Запуск Flask-сервера і aiogram webhook
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=f'/{BOT_TOKEN}',
+        on_start=on_start,
+        host="0.0.0.0",
+        port=5000,
+    )
